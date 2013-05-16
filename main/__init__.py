201206b2f70523fdaa0b80d9c9f31427615608b6
Luke Benstead <Luke Benstead lukeb@potatolondon.com>
2013-05-16 11:15:56 +0100

    Massive code cleanup. REMOVING PYTHON 2.5 SUPPORT

--- main/__init__.py
+++ main/__init__.py
@@ -1,32 +1,5 @@
-import os
-import sys
-
-
-# Add parent folder to sys.path, so we can import boot.
-# App Engine causes main.py to be reloaded if an exception gets raised
-# on the first request of a main.py instance, so don't add project_dir
-# multiple times.
-project_dir = os.path.abspath(
-    os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-if project_dir not in sys.path or sys.path.index(project_dir) > 0:
-    while project_dir in sys.path:
-        sys.path.remove(project_dir)
-    sys.path.insert(0, project_dir)
-
-for path in sys.path[:]:
-    if path != project_dir and os.path.isdir(os.path.join(path, 'django')):
-        sys.path.remove(path)
-        break
-
-# Remove the standard version of Django.
-if 'django' in sys.modules and sys.modules['django'].VERSION < (1, 2):
-    for k in [k for k in sys.modules
-              if k.startswith('django.') or k == 'django']:
-        del sys.modules[k]
-
 from djangoappengine.boot import setup_env
 setup_env()
-
 
 def validate_models():
     """
@@ -58,20 +31,15 @@ from djangoappengine.utils import on_production_server
 if not on_production_server:
     validate_models()
 
-from django.conf import settings
-
-def log_traceback(*args, **kwargs):
-    import logging
-    logging.exception("Exception in request:")
-
-from django.core import signals
-signals.got_request_exception.connect(log_traceback)
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Add the staticfiles handler if necessary.
-if settings.DEBUG and 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+from django.conf import settings
+
+## In vanilla Django, staticfiles overrides runserver to use StaticFilesHandler
+## if necessary. As we can't do this in our runserver (because we handover to dev_appserver)
+## this has to be done here
+if (not on_production_server and settings.DEBUG) and 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
     from django.contrib.staticfiles.handlers import StaticFilesHandler
     application = StaticFilesHandler(application)
 
