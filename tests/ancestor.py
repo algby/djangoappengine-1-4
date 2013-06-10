b2e3289c35322bbec8159345e7f9fb106674edd6
Luke Benstead <Luke Benstead lukeb@potatolondon.com>
2013-06-10 10:11:56 +0100

    More ancestor query tests and fixes

--- tests/ancestor.py
+++ tests/ancestor.py
@@ -17,10 +17,11 @@ class AncestorQueryTest(TestCase):
         parent = AncestorModel.objects.create()
         ChildModel.objects.create(id=1)
 
-        child = ChildModel.objects.create(id=AncestorKey(parent))
+        child = ChildModel.objects.create(id=AncestorKey(parent, 2))
 
         self.assertTrue(child.pk) #Should be populated
         self.assertTrue(isinstance(child.pk, (int, long)))
+        self.assertEqual(2, child.pk) #Should be the ID we passed in
 
         self.assertTrue(child.parent())
         self.assertEqual(parent, child.parent())
@@ -36,7 +37,20 @@ class AncestorQueryTest(TestCase):
         self.assertEqual(1, child_count)
 
         #Create another child
-        ChildModel.objects.create(id=AncestorKey(parent))
+        child2 = ChildModel.objects.create(id=AncestorKey(parent))
 
         child_count = ChildModel.descendents_of(parent).filter(field1="apples").count()
         self.assertEqual(2, child_count)
+
+        #Check that we can still get the children by their ID like normal
+        pk = child.pk
+        child = ChildModel.objects.get(pk=pk)
+        self.assertEqual(pk, child.pk)
+
+        pk = child2.pk
+        child2 = ChildModel.objects.get(pk=pk)
+        self.assertEqual(pk, child2.pk)
+
+        #Check all children are returned without the ancestor query
+        child_count = ChildModel.objects.filter(field1="apples").count()
+        self.assertEqual(3, child_count)
