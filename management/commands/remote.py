cd4687cad3fa684288365848038acb55a41ea5d2
Luke Benstead <Luke Benstead lukeb@potatolondon.com>
2013-06-19 14:36:32 +0100

    Properly set environment variables in the remote command

--- management/commands/remote.py
+++ management/commands/remote.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management import execute_from_command_line
 from django.core.management.base import BaseCommand
 
@@ -15,5 +17,16 @@ class Command(BaseCommand):
             if isinstance(connection, DatabaseWrapper):
                 stub_manager.setup_remote_stubs(connection)
                 break
+
+        from djangoappengine.boot import PROJECT_DIR
+        app_yaml = open(os.path.join(PROJECT_DIR, "app.yaml")).read()
+
+        app_version = app_yaml.split("version:")[1].lstrip().split()[0]
+        app_name = app_yaml.split("application:")[1].lstrip().split()[0]
+
+        os.environ['DEFAULT_VERSION_HOSTNAME'] = "%s.appspot.com" % app_name
+        os.environ['APPLICATION_ID'] = "s~" + app_name
+        os.environ['CURRENT_VERSION_ID'] = app_version
+
         argv = argv[:1] + argv[2:]
         execute_from_command_line(argv)
