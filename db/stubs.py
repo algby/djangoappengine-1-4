ee08980c87303365a4d543a59fdda0c25774dd1f
Luke Benstead <Luke Benstead lukeb@potatolondon.com>
2014-03-31 11:17:04 +0100

    Fix up imports broken in 1.9.1

--- db/stubs.py
+++ db/stubs.py
@@ -82,7 +82,7 @@ class StubManager(object):
         args.update(connection.settings_dict.get('DEV_APPSERVER_OPTIONS', {}))
         log_level = logging.getLogger().getEffectiveLevel()
         logging.getLogger().setLevel(logging.WARNING)
-        from google.appengine.tools import dev_appserver
+        from google.appengine.tools import old_dev_appserver as dev_appserver
         dev_appserver.SetupStubs('dev~' + appid, **args)
         logging.getLogger().setLevel(log_level)
         self.active_stubs = 'local'
