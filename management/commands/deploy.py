ae4993597f5afcfa0df42f0fa50913f4c85e2b74
Luke Benstead <lukeb@potatolondon.com>
2014-09-18 13:41:06 +0100

    Read the app.yaml version after pre-deploy, incase a pre-deploy command changes it

--- management/commands/deploy.py
+++ management/commands/deploy.py
@@ -72,10 +72,6 @@ class Command(BaseCommand):
 
     def run_from_argv(self, argv):
         from djangoappengine.boot import PROJECT_DIR
-        app_yaml = open(os.path.join(PROJECT_DIR, "app.yaml")).read()
-
-        app_version = app_yaml.split("version:")[1].lstrip().split()[0]
-        app_name = app_yaml.split("application:")[1].lstrip().split()[0]
 
         for command in PRE_DEPLOY_COMMANDS:
             if isinstance(command, (list, tuple)):
@@ -87,6 +83,11 @@ class Command(BaseCommand):
                     call_command(command[0], *command[1], **command[2])
             else:
                 call_command(command)
+
+        app_yaml = open(os.path.join(PROJECT_DIR, "app.yaml")).read()
+
+        app_version = app_yaml.split("version:")[1].lstrip().split()[0]
+        app_name = app_yaml.split("application:")[1].lstrip().split()[0]
 
         os.environ['DEFAULT_VERSION_HOSTNAME'] = "%s.appspot.com" % app_name
         os.environ['APPLICATION_ID'] = "s~" + app_name
